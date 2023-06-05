341c5508bf40b665a6a432ab59f39f20a870e7fc
unknown <131753147+cfernandez-create@users.noreply.github.com>
2023-06-04 22:59:05 -0700

    redux setup and products fetched from api

--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,20 +1,55 @@
-import {Center, Wrap, WrapItem} from '@chakra-ui/react'
-import {products} from '../products'
-import ProductCard from '../components/ProductCard'
+import {
+  Center, 
+  Wrap, 
+  WrapItem, 
+  Stack, 
+  Alert, 
+  AlertIcon, 
+  AlertDescription, 
+  AlertTitle 
+} from "@chakra-ui/react";
+import ProductCard from "../components/ProductCard";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../redux/actions/productActions";
+import { useEffect } from "react";
+import { Spinner } from '@chakra-ui/react'
+
+
+
 
 const ProductsScreen = () => {
-  return ( 
-   <Wrap spacing='30px' justifyContent='center' minHeight='100vh'>
+  const dispatch = useDispatch();
 
-    {products.map((product) => (
-        <WrapItem key={product._id}>
-            <Center w='250px' h='550px'>
-               <ProductCard product={product}/>
+  const productList = useSelector((state) => state.products);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  return (
+    <Wrap spacing="30px" justifyContent="center" minHeight="100vh">
+      {loading ? (
+      <Stack direction='row' spacing={4}>
+        <Spinner  mt={20} thickness="2px" speed='0.65s' emptyColor='gray.200' color='orange.500' size='x1'/>
+      </Stack>
+      ) : error ? (
+        <Alert status='error'>
+        <AlertIcon/>
+        <AlertTitle>We are sorry!</AlertTitle>
+        <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      ) : (
+        products.map((product) => (
+          <WrapItem key={product._id}>
+            <Center w="250px" h="550px">
+              <ProductCard product={product} />
             </Center>
-        </WrapItem>
-    ))}
-   </Wrap>
-  )
-}
+          </WrapItem>
+        ))
+      )}
+    </Wrap>
+  );
+};
 
-export default ProductsScreen
+export default ProductsScreen;
